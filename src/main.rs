4b63a110466a27461f467d5529dae4257d83b34f
Tim Spence <timothywspence@gmail.com>
2023-10-03 15:28:10 +0100

    Parse request line

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 // Uncomment this block to pass the first stage
+use anyhow::{anyhow, Result};
+use std::io::BufRead;
+use std::io::BufReader;
 use std::io::Write;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,15 +15,41 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(mut tx) => {
                 println!("accepted new connection");
-                stream
-                    .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-                    .unwrap();
+                let mut rx = BufReader::new(tx.try_clone().unwrap());
+                let req_line = parse_request_line(&mut rx).unwrap();
+                match req_line.path.as_ref() {
+                    "/" => tx.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap(),
+                    _ => tx.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap(),
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn parse_request_line(s: &mut BufReader<TcpStream>) -> Result<RequestLine> {
+    let mut buf = String::new();
+    s.read_line(&mut buf)?;
+    let (init, r) = &buf.split_at(buf.len());
+    assert_eq!(r, &"\r");
+    match init.to_lowercase() {
+        x if x.starts_with("get ") => Ok(RequestLine {
+            method: Method::Get,
+            path: x[4..].to_string(),
+        }),
+        _ => Err(anyhow!("invalid request")),
+    }
+}
+
+struct RequestLine {
+    method: Method,
+    path: String,
+}
+
+enum Method {
+    Get,
 }
